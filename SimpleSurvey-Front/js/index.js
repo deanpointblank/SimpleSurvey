3513963a6383a5586eb0b8897663021ad43c33a1
dean <deanlloyd64@gmail.com>
2020-01-20 21:37:36 -0500

    create JS variables for fetch URLS

---
+++ SimpleSurvey-Front/js/index.js
@@ -0,0 +1,9 @@
+// URL variables for 'fetch'
+const BASE_URL = 'http://localhost:3000'
+const USER_URL = `${BASE_URL}/users`
+const SURVEY_URL = `${BASE_URL}/surveys`
+const QUESTIONS_URL = `${BASE_URL}/questions`
+
+// Elemant variables to append to
+const main = document.querySelector('main')
+
