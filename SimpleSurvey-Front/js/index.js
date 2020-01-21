af3fff2aa08f0856e383857decb672a6b892a064
dean <deanlloyd64@gmail.com>
2020-01-20 22:03:46 -0500

    begin fetch calls on contentr load

--- SimpleSurvey-Front/js/index.js
+++ SimpleSurvey-Front/js/index.js
@@ -1,9 +1,14 @@
 // URL variables for 'fetch'
 const BASE_URL = 'http://localhost:3000'
-const USER_URL = `${BASE_URL}/users`
+const USERS_URL = `${BASE_URL}/users`
 const SURVEY_URL = `${BASE_URL}/surveys`
 const QUESTIONS_URL = `${BASE_URL}/questions`
 
 // Elemant variables to append to
 const main = document.querySelector('main')
 
+document.addEventListener("DOMContentLoaded", () => {
+    fetch(USERS_URL)
+    .then(resp => resp.json())
+    .then(users => {    })
+})
