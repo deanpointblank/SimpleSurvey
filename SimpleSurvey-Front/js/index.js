97032c85cf2c706ff993c728c09cb6f2234ad70e
dean <deanlloyd64@gmail.com>
2020-01-21 21:41:05 -0500

    add skeletal test forms to front end with minil=mal functionality

--- SimpleSurvey-Front/js/index.js
+++ SimpleSurvey-Front/js/index.js
@@ -8,25 +8,60 @@ const QUESTIONS_URL = `${BASE_URL}/questions`
 const main = document.querySelector('main')
 const userMenu = document.querySelector('.userMenu')
 const li = document.createElement('li')
+const mosaic = document.querySelector('.card-columns')
+const card = document.createElement('div')
 
 // initial fetch request to populate users
 document.addEventListener("DOMContentLoaded", () => {
     fetch(USERS_URL)
-    .then(resp => resp.json())
-    .then(users => {
-        const usersArray = users['data']
-        console.log(usersArray)
-        for(const user of usersArray){
-            li.innerHTML = `<a href="#">${user['attributes']['name']}</a>`
-            userMenu.appendChild(li)
-        }
-    })
+        .then(resp => resp.json())
+        .then(users => {
+            const usersArray = users['data']
+            console.log(usersArray)
+            for (const user of usersArray) {
+                li.innerHTML = `<a href="#">${user['attributes']['name']}</a>`
+                userMenu.appendChild(li)
+            }
+        });
+    addSurveys();
 })
 
+// fetch to populate survey mosaic
+function addSurveys(){
+    fetch(SURVEY_URL)
+    .then(resp => resp.json())
+    .then(surveys => {
+        const surveysArray = surveys['data']
+        console.log(surveysArray)
+        for (const survey of surveysArray){
+            mosaic.innerHTML += `
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">${survey['attributes']['name']}</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.
+                    </p>
+                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                </div>
+            </div>
+            `
+        }
+    })
+}
+
+
+// miscelaneous 
 function openForm() {
     document.getElementById("myForm").style.display = "block";
-  }
-  
-  function closeForm() {
+}
+
+function closeForm() {
     document.getElementById("myForm").style.display = "none";
-  }
+}
+
+function on() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
+}
