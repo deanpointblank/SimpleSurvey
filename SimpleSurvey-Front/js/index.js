062790989682d956bb545b42b835099edda76b69
dean <deanlloyd64@gmail.com>
2020-01-22 15:09:47 -0500

    add funtionality to generate survey questions

--- SimpleSurvey-Front/js/index.js
+++ SimpleSurvey-Front/js/index.js
@@ -8,10 +8,11 @@ const QUESTIONS_URL = `${BASE_URL}/questions`
 const main = document.querySelector('main')
 const userMenu = document.querySelector('.userMenu')
 const mosaic = document.querySelector('.card-columns')
+const questions = document.querySelector('.questions')
 
 // Create elements
 const li = document.createElement('li')
-const card = document.createElement('div')
+const div = document.createElement('div')
 
 
 // initial fetch request to populate users
@@ -30,14 +31,14 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 
 // fetch to populate survey mosaic
-function addSurveys(){
+function addSurveys() {
     fetch(SURVEY_URL)
-    .then(resp => resp.json())
-    .then(surveys => {
-        const surveysArray = surveys['data']
-        console.log(surveysArray)
-        for (const survey of surveysArray){
-            mosaic.innerHTML += `
+        .then(resp => resp.json())
+        .then(surveys => {
+            const surveysArray = surveys['data']
+            console.log(surveysArray)
+            for (const survey of surveysArray) {
+                mosaic.innerHTML += `
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">${survey['attributes']['name']}</h5>
@@ -47,44 +48,67 @@ function addSurveys(){
                 </div>
             </div>
             `
-        }
-    })
+            }
+        })
 }
 
 // add/delete question fields to form
-function addQuestionField(){
+function addQuestionField() {
+    questions.innerHTML += `
+    <div class="form-group" id="${questions.children.length}"><br />
+        <label for="question">Question Name</label>
+        <input type="text" class="form-control"></input>
+        <label for="questionType">Question Type</label><br>
+        <button type="button" class="btn btn-secondary" onclick="addMultipleChoiceValues(${questions.children.length})"> Multiple Choice </button>
+        <button type="button" class="btn btn-secondary" onclick="addTrueFalseValues(${questions.children.length})"> True or False </button>
+        <button type="button" class="btn btn-secondary" onclick="addTextValue(${questions.children.length})"> Open Ended </button>
+        <button type="button" class="btn btn-secondary" onclick="removeQuestionField(${questions.children.length})"> remove question </button>
+    </div>
+    `
     // <label for="psw"><b>description</b></label>
     // <input type="password" placeholder="Enter Password" name="psw" required>
     // <br />
+    console.log('clicked!')
 }
 
+function removeQuestionField(childnumber){
+    questions.children[childnumber].remove()
+}
+
+function addMultipleChoiceValues(childnumber){
+    console.log(questions.children[childnumber])
+}
+function addTrueFalseValues(childnumber){
+    console.log(questions.children[childnumber])
+}
+function addTextValue(childnumber){
+    console.log(questions.children[childnumber])
+}
 
 // miscelaneous 
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
-}
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+// }
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
+// function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+// }
 
-function on() {
-    document.getElementById("overlay").style.display = "block";
-}
+// function on() {
+//     document.getElementById("overlay").style.display = "block";
+// }
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
+// function off() {
+//     document.getElementById("overlay").style.display = "none";
+// }
 
-//------------
-/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+/* Set the width of the sidebar */
+function nav() {
+    if (document.getElementById("mySidebar").style.width === "250px") {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    } else {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    }
 }
